f9b1fb106f7679feae04ab7db94d4c65badbe954
Todd Whitelow <todd@whitelow.tech>
2020-08-31 18:40:39 -0600

    contact update

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,1 +1,10 @@
+class Contact
+  attr_accessor :first_name, :last_name, :email, :phone_number
 
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @email = [:email]
+    @phone_number = [:phone_number]
+  end
+end
